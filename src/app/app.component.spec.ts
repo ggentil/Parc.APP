ad2fa40a728e4c1172e78c5cb1cc5dc02c636790
Guilherme Gentil <guilherme.gentil@e-construmarket.com.br>
2018-11-23 14:36:58 -0200

    Doing unit testing
    Just starting, simple tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
+import { NgxCurrencyModule } from 'ngx-currency';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'parcapp'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('parcapp');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to parcapp!');
-  }));
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [
+            AppComponent
+        ],
+        imports: [
+          BrowserModule,
+          FormsModule,
+          ReactiveFormsModule,
+          NgxCurrencyModule
+        ]
+        }).compileComponents();
+    }));
+
+    it('Deve criar o app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+
+    it(`Modo padrão deve ser 'arredondado'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.modo).toEqual('arredondado');
+    }));
+
+    it('deve renderizar os campos "texts" para inserção de dados', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('#valor') && compiled.querySelector('#numeroParcelas')).toBeTruthy();
+    }));
+
 });
